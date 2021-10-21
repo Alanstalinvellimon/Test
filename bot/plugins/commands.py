f61f220d7c872b9343f7beb85d2052cbc93013e9
Alanstalinvellimon <92779268+Alanstalinvellimon@users.noreply.github.com>
2021-10-21 21:46:05 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -59,9 +59,11 @@ async def start(bot, update):
     
     Send_message = await bot.send_photo( chat_id=update.chat.id,
  photo="https://telegra.ph/file/fd29a8d6e30223503ed7f.jpg",
- caption="<b>Couldn't Find This Movie.Try Again..! ഈ സിനിമയുടെ ഒറിജിനൽ പേര് ഗൂഗിളിൽ പോയി കണ്ടെത്തി അതുപോലെ ഇവിടെ കൊടുക്കുക 🥺</b>", reply_to_message_id=update.message_id )
+ caption="<b>Couldn't Find This Movie.Try Again..! ഈ സിനിമയുടെ ഒറിജിനൽ പേര് ഗൂഗിളിൽ പോയി കണ്ടെത്തി അതുപോലെ ഇവിടെ കൊടുക്കുക 🥺</b>"),
+ 
   reply_markup = InlineKeyboardMarkup(buttons)
-    
+    ]]
+
 @Client.on_message(filters.command(["help"]) & filters.private, group=1)
 async def help(bot, update):
     buttons = [[
